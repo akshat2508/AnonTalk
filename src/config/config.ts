dac7ceff7b800c321e282d64b225572d75ae4de3
Akshat-paul <akshatpaul2006@gmail.com>
2025-06-14 12:07:01 +0530

    Updated

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,8 @@
 
-export const supabaseUrl = process.env.SUPABASE_URL!; // Replace with your URL
-export const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
+// export const supabaseUrl = process.env.SUPABASE_URL!; // Replace with your URL
+// export const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
+
+import Constants from 'expo-constants';
+
+export const supabaseUrl = Constants.expoConfig?.extra?.SUPABASE_URL!;
+export const supabaseAnonKey = Constants.expoConfig?.extra?.SUPABASE_ANON_KEY!;
