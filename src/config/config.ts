471d289c6fa9c7b7a90eef999b1e47c6a085c7d7
Akshat-paul <akshatpaul2006@gmail.com>
2025-06-14 20:53:27 +0530

    check commit

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,3 @@
 
-// export const supabaseUrl = process.env.SUPABASE_URL!; // Replace with your URL
-// export const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
-
-
 export const supabaseUrl = 'https://gpaigfioahcpzixczdjj.supabase.co'; // Replace with your URL
-export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwYWlnZmlvYWhjcHppeGN6ZGpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyNzA3MjMsImV4cCI6MjA2NDg0NjcyM30.Tb9QRetXx4CLkoH9xZylovvDuzIphKl8_xKnO2tCFTg'; // Replace with your key
+export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwYWlnZmlvYWhjcHppeGN6ZGpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyNzA3MjMsImV4cCI6MjA2NDg0NjcyM30.Tb9QRetXx4CLkoH9xZylovvDuzIphKl8_xKnO2tCFTg'; 
