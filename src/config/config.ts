3a6e41f3d048655f992299887fccfeeec4658f4f
Akshat-paul <akshatpaul2006@gmail.com>
2025-06-14 20:57:10 +0530

    deleted config.ts

--- src/config/config.ts
+++
@@ -1,3 +0,0 @@
-
-export const supabaseUrl = 'https://gpaigfioahcpzixczdjj.supabase.co'; // Replace with your URL
-export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwYWlnZmlvYWhjcHppeGN6ZGpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyNzA3MjMsImV4cCI6MjA2NDg0NjcyM30.Tb9QRetXx4CLkoH9xZylovvDuzIphKl8_xKnO2tCFTg'; 
