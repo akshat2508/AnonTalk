3e18246661e20fd199e4a6c810b1f0e30c518e7c
Akshat-paul <akshatpaul2006@gmail.com>
2025-10-09 13:48:33 +0530

    Updated environment variables accessibility

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+
+
+// export const supabaseUrl = ''; // Replace with your URL
+// export const supabaseAnonKey = ''; 
+
+
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@env";
+
+export const supabaseUrl = SUPABASE_URL;
+export const supabaseKey = SUPABASE_ANON_KEY;
